0b55e1b815b5e80344a93c33ddbf9fa08e88af9f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-03 14:08:33 +0900

    do not generate assets and helpers

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,10 @@ module TodoMinder
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join('node_modules')
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
