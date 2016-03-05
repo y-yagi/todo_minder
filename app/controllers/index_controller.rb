c9bf0307a9f5f5a76f9071af621093629cca1c2c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-05 12:43:04 +0900

    redirect to tasks path when logged in

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,5 +2,6 @@ class IndexController < ApplicationController
   skip_before_action :check_logged_in
 
   def index
+    redirect_to tasks_path if logged_in?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth_hash(auth_hash)
     @user.update_if_needed!(auth_hash)
     session[:user_id] = @user.id
-    redirect_to root_path
+    redirect_to tasks_path
   end
 
   def destroy
