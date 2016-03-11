278ad72247134b03b4bcdd7efa6f408eb688121a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-11 13:29:51 +0900

    add finished to tasks

--- db/migrate/20160305042712_create_tasks.rb
+++ db/migrate/20160305042712_create_tasks.rb
@@ -1,11 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :detail
+      t.string :detail, null: false
       t.references :list, foreign_key: true
       t.references :user, foreign_key: true
       t.datetime :deadline_at
       t.integer :point
+      t.boolean :finished, default: false
 
       t.timestamps
     end
