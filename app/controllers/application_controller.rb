dfc7fc1c5c3764fa8b5b470ea581d21e3458040a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-19 22:21:56 +0900

    display logout link only logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :check_logged_in
   force_ssl if: :ssl_configured?
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
