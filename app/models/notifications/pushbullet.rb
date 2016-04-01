01122473b9c790d0047ee934799e871c6f663da8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-04-02 08:52:07 +0900

    push to all device

--- app/models/notifications/pushbullet.rb
+++ app/models/notifications/pushbullet.rb
@@ -8,8 +8,10 @@ module Notifications
         ::Pushbullet.api_token = Rails.application.secrets.pushbullet_key
         devices = ::Pushbullet::Device.all
 
-        Task.deadline_is_close(user).each do |task|
-          devices.first.push_link(TASK_TITLE, Rails.configuration.app["host"], task.detail)
+        devices.each do |device|
+          Task.deadline_is_close(user).each do |task|
+            device.push_link(TASK_TITLE, Rails.configuration.app["host"], task.detail)
+          end
         end
       end
     end
