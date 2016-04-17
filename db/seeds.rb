10753855e298e94b10b0dd8a55edffe4179ec3f4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-04-17 14:31:05 +0900

    add script to generate sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+list_ids = List.pluck(:id)
+days = (1..10).to_a
+hiragana = ("あ".."ん").to_a
+
+30.times do |_|
+  detail = rand(1..20).times.map { hiragana[rand(hiragana.size)] }.join
+  Task.create!(
+    detail: detail,
+    list_id: list_ids.sample,
+    user: User.first,
+    deadline_at: rand(1..10).days.since
+  )
+end
