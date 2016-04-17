2c5950a4e950a3196b0990566301916f18836917
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-04-17 14:17:01 +0900

    remove needless comments

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,26 +2,21 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :set_lists, only: [:index, :create, :edit, :update, :destroy]
 
-  # GET /tasks
   def index
     @task = Task.new
   end
 
-  # GET /tasks/1
   def show
   end
 
-  # GET /tasks/new
   def new
     @list = current_user.lists.find(params[:list_id])
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
   def create
     @task = Task.build(task_params, current_user)
 
@@ -32,7 +27,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
       redirect_to tasks_path, notice: 'Task was successfully updated.'
@@ -41,7 +35,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
