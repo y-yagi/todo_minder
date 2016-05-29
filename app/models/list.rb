91884049471a22ac5925311eedcfa03ea9488d03
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-05-30 08:24:38 +0900

    remove '今日やること'
    Todays task management in status.

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,7 +7,6 @@ class List < ApplicationRecord
   class << self
     def generate_default_lists!(user)
       # TODO: i18n対応は後から考える
-      List.create!(name: '今日やること', user: user)
       List.create!(name: '仕事', user: user)
       List.create!(name: '個人', user: user)
     end
