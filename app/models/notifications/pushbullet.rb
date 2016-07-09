7c55f4178d00aaa0091980b6fc78eba2456587be
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-09 14:03:57 +0900

    ping to server

--- app/models/notifications/pushbullet.rb
+++ app/models/notifications/pushbullet.rb
@@ -3,13 +3,16 @@ module Notifications
     TASK_TITLE = 'TaskMinder'.freeze
     class << self
       def run
+        host = Rails.configuration.app["host"]
+        # NOTE: start the server
+        Net::HTTP.get(URI.parse(host))
         User.where(notification: true).each do |user|
           ::Pushbullet.api_token = user.raw_pushbullet_api_token
           devices = ::Pushbullet::Device.all
 
           devices.each do |device|
             Task.deadline_is_close(user).each do |task|
-              device.push_link(TASK_TITLE, Rails.configuration.app["host"], task.detail)
+              device.push_link(TASK_TITLE, host, task.detail)
             end
           end
           ::Pushbullet.api_token = nil
